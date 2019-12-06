f9613566dfb36eb7b8d40cab886f187a39b3e051
Lars Kellogg-Stedman <lars@redhat.com>
2019-12-06 12:15:29 -0500

    comment up deploy script

--- roles/prepare_overcloud_deploy/files/overcloud-deploy.sh
+++ roles/prepare_overcloud_deploy/files/overcloud-deploy.sh
@@ -5,14 +5,30 @@ TEMPLATES=/usr/share/openstack-tripleo-heat-templates
 deploy_args=(
 	-n network_data.yaml
 
+	# Provide information about fetching container images
 	-e containers-prepare-credentials.yaml
 	-e containers-prepare-parameter.yaml
+
+	# Node flavors and counts for the overcloud
 	-e templates/node-info.yaml
+
+	# Add the undercloud ca certificate to overcloud nodes
 	-e templates/inject-trust-anchor-hiera.yaml
+
+	# Enable HA services (pacemaker)
+	-e $TEMPLATES/environments/docker-ha.yaml
+
+	# Enable network isolation (different networks for
+	# different purposes) and describe the network
+	# configuration of the overcloud nodes.
 	-e $TEMPLATES/environments/network-isolation.yaml
 	-e $TEMPLATES/environments/network-environment.yaml
-	-e $TEMPLATES/environments/docker-ha.yaml
 	-e templates/custom-networks.yaml
+
+	# Pacemaker fencing configuration
+	-e templates/fencing.yaml
+
+	# Misc configuration settings
 	-e templates/deploy.yaml
 )
 
